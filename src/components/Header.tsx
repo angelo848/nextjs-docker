5290172aec529987cde9de28be35d6c701887649
Angelo Salles <angelo_848@live.com>
2023-05-04 23:11:11 -0300

    header almost finished

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,22 @@
-import { Avatar, Button, Dropdown, Navbar } from 'flowbite-react'
-import Link from "next/link";
+import { Navbar } from "flowbite-react"
+import Image from "next/image";
 
 export default function Header() {
   return (
-    <Navbar fluid rounded>
-      <Navbar.Brand href='#nav'>
-        <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+    <Navbar fluid className="fixed top-0 left-0 w-full !bg-main/50 !py-0">
+      <Navbar.Brand href="#">
+        <img src="/logo.png" className="h-36 sm:h-60 md:h-40" />
+        {/* <Image src="/logo.png" className="h-6 sm:h-9" alt="Flowbite Logo" width={50} height={100} /> */}
+        {/* <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Priscila </span> */}
       </Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse>
-        <Navbar.Link href="#nav" active>
-          Home
-        </Navbar.Link>
-        <Navbar.Link href='#nav'>
-          About
-        </Navbar.Link>
-        <Navbar.Link href="#nav">Services</Navbar.Link>
-        <Navbar.Link href="#nav">Pricing</Navbar.Link>
-        <Navbar.Link href="#nav">Contact</Navbar.Link>
+        <Navbar.Link href="#nav" className="hover:!text-tertiary">O que é uma doula?</Navbar.Link>
+        <Navbar.Link href="#nav" className="hover:!text-tertiary">Quem sou eu</Navbar.Link>
+        <Navbar.Link href="#nav" className="hover:!text-tertiary">Meu trabalho</Navbar.Link>
+        <Navbar.Link href="#nav" className="hover:!text-tertiary">Como contratar?</Navbar.Link>
       </Navbar.Collapse>
+      <div></div>
     </Navbar>
   )
 }
